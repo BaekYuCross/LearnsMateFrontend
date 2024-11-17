df089395bba72e48c9026d5275468a19034f2da7
Hellin22 <dno06117@naver.com>
2024-11-17 19:06:47 +0900

    ♻ 코드 수정: MemberView conflict 수정

--- src/views/MemberView.vue
+++ src/views/MemberView.vue
@@ -17,15 +17,6 @@ import MemberFilter from '@/components/member/MemberFilter.vue';
     padding: 20px;
   }
   </style>
-<template>
-    <div class="member-container">
-      <MemberSideMenu/>
-    </div>
-  </template>
-  
-  <script setup>
-    import MemberSideMenu from '@/components/sideMenu/MemberSideMenu.vue';
-  </script>
   
   <style scoped>
   .member-container {
