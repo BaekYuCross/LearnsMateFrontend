ca576d8cd4f905ad731938c42daaedc2906943aa
DESKTOP-BC2AP8U\Playdata <cho9759@gmail.com>
2024-11-18 14:34:27 +0900

    ♻️ 코드 수정: router.js voc 파일명 수정

--- src/router/router.js
+++ src/router/router.js
@@ -24,7 +24,7 @@ const routes = [
   {
     path: '/voc',
     name: 'VOC',
-    component: () => import('../views/VocView.vue')
+    component: () => import('../views/VOCView.vue')
   },
   {
     path: '/',
