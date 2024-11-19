65ca4fed6d643cac75a6a3af28ef7a2635dde896
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-11-19 10:29:33 +0900

    ✨ 기능 추가: 등록 모듈창 컴포넌트 구현

--- src/router/login.js
+++ src/router/login.js
@@ -4,6 +4,21 @@ const LoginRouter = [
         name: 'Login',
         component: () => import('../views/login/LoginView.vue')
     },
+    {
+        path: '/modal',
+        name: 'modal',
+        component: () => import('../components/modules/DeleteModule.vue')
+    },
+    {
+        path: '/modal2',
+        name: 'modal2',
+        component: () => import('../components/modules/FalseModule.vue')
+    },
+    {
+        path: '/modal3',
+        name: 'modal3',
+        component: () => import('../components/modules/RegisterModule.vue')
+    },
 ];
 
 export default LoginRouter;
