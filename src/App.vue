02671a75e0ba96d86e1f6fff29572e253b64ecaf
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-11-23 20:39:58 +0900

    ✨ 기능 추가: 내강의 페이지 헤더 숨김 기능 추가

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,7 @@ const isClientMainPage = computed(() => route.path === '/client-main');
 const isClientVocPage = computed(() => route.path === '/client-voc');
 const isClientMyVocPage = computed(() => route.path === '/client-myvoc');
 const isClientLecturePage = computed(() => route.path === '/client-lecture');
+const isClientMyLecturePage = computed(() => route.path === '/client-mylecture');
 
 
 
