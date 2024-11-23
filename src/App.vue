2a9e2c3f2fd9c25c7c3078ce358a4597f53c13e1
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-11-23 20:37:58 +0900

    ✨ 기능 추가: 메인 페이지 헤더 숨김 기능 추가

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <AppHeader v-if="showHeader" />
+    <AppHeader v-if="showHeader && !isTransitioning" />
   </div>
   <main>
     <RouterView />
@@ -8,21 +8,35 @@
 </template>
 
 <script setup>
-
 import AppHeader from './components/AppHeader.vue';
-import { RouterView , useRoute} from 'vue-router';
-import { computed } from 'vue';
+import { RouterView, useRoute } from 'vue-router';
+import { computed, ref, watch } from 'vue';
 
 const route = useRoute();
+const isTransitioning = ref(false);
 const isLoginPage = computed(() => route.path === '/login');
 const isClientLoginPage = computed(() => route.path === '/client-login');
+const isClientMainPage = computed(() => route.path === '/client-main');
+
+
 
 const showHeader = computed(() => {
-  return !isLoginPage.value && !isClientLoginPage.value;
+  return !isLoginPage.value && !isClientLoginPage.value 
+  && !isClientMainPage.value && !isClientVocPage.value 
+  && !isClientMyVocPage.value && !isClientLecturePage.value 
+  && !isClientMyLecturePage.value && !isClientLectureDetailPage.value;
 });
 
+watch(route, () => {
+  isTransitioning.value = true;
+  isTransitioning.value = false;
+ 
+});
 </script>
 
-<style scoped>
-
+<style>
+/* 초기 상태에서 AppHeader 숨김 */
+div[style*="display: none"] {
+  display: none !important;
+}
 </style>
