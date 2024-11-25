489589fde6307f77fd2e3cecd8601b5441277f00
Hellin22 <dno06117@naver.com>
2024-11-25 21:41:01 +0900

    ✨기능 추가: meta 데이터 만들어서 client 이용하는 창인걸 알리기

--- src/router/client.js
+++ src/router/client.js
@@ -3,55 +3,64 @@ const ClientRouter = [
         path: '/client-login',
         name: 'ClientLogin',
         component: () => import('../views/client/clientlogin/ClientLoginView.vue'),
-        props: true
+        props: true,
+        meta: { isClientPage: false }
     },
     {
         path: '/client-main',
         name: 'ClientMain',
         component: () => import('../views/client/clientmain/ClientMainView.vue'),
-        props: true
+        props: true,
+        meta: { isClientPage: true }
     },
     {
         path: '/client-voc',
         name: 'ClientVoc',
         component: () => import('../views/client/clientvoc/ClientVOCView.vue'),
-        props: true
+        props: true,
+        meta: { isClientPage: true }
     },
     {
         path: '/client-myvoc',
         name: 'ClientMyVoc',
         component: () => import('../views/client/clientvoc/ClientVOCMyView.vue'),
-        props: true
+        props: true,
+        meta: { isClientPage: true }
     },
     {
         path: '/client-lecture',
         name: 'ClientLecture',
         component: () => import('../views/client/clientlecture/ClientLectureView.vue'),
-        props: true
+        props: true,
+        meta: { isClientPage: true }
     },
     {
         path: '/client-mylecture',
         name: 'ClientMyLecture',
         component: () => import('../views/client/clientlecture/ClientMyLectureView.vue'),
-        props: true
+        props: true,
+        meta: { isClientPage: true }
     },
     {
         path: '/client-lecturedetail/:id',
         name: 'ClientLectureDetail',
         component: () => import('../views/client/clientlecture/ClientLectureDetail.vue'),
-        props: true
+        props: true,
+        meta: { isClientPage: true }
     },
     {
         path: '/client-lecturecart',
         name: 'ClientLectureCart',
         component: () => import('../views/client/clientlecture/ClientLectureCart.vue'),
-        props: true
+        props: true,
+        meta: { isClientPage: true }
     },
     {
         path: '/client-addcoupon',
         name: 'AddCoupon',
         component: () => import('../views/client/clientlecture/ClientCouponAddView.vue'),
-        props: true
+        props: true,
+        meta: { isClientPage: true }
     },
 ];
 
