36dac5537a83ef8f3aade2df6b91e8bc2641ab5f
Hellin22 <dno06117@naver.com>
2024-11-25 21:41:18 +0900

    ✨기능 추가: 활동이 없을시 띄울 모달창 추가

---
+++ src/components/client/InactivityModal.vue
@@ -0,0 +1,20 @@
+<template>
+    <div v-if="show" class="inactivity-modal">
+      <div class="modal-content">
+        <h2>사용 중이신가요?</h2>
+        <p>장시간 움직임이 감지되지 않았습니다.</p>
+        <div class="modal-buttons">
+          <button @click="$emit('continue')">예</button>
+          <button @click="$emit('logout')">아니오</button>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  defineProps({
+    show: Boolean
+  });
+  
+  defineEmits(['continue', 'logout']);
+  </script>
