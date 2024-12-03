060cf2f6435e8a00144b9360ee836aa5f8d68ee6
sksmsdlskgus <lnh03280@g.eulji.ac.kr>
2024-12-03 15:46:02 +0900

    💄 UI/스타일 파일 수정: 결제내역으로 가면 헤더 선택 풀림 수정

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -56,8 +56,7 @@ const timer = ref(null);
 
 const menus = ref([
   { name: '메인', path: '/main', group: 'main' },
-  { name: '강의', path: '/lecture', group: 'lecture' },
-  { name: '결제', path: '/payment', group: 'payment' },
+  { name: '강의', path: '/lecture', group: 'lecture' , includePaths: ['/payment']},
   { 
     name: '고객', 
     path: '/student', 
